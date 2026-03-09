9d73ac6ed7f615ac46f0290102633ecd7fe19ae2
GouravMankar <gouravmankar@hotmail.com>
2026-03-10 00:36:56 +0530

    [Gourav Mankar] : Ability to add multiple person to Address Book

--- src/main/java/com/apps/addressbooksystem/AddressBookApplication.java
+++ src/main/java/com/apps/addressbooksystem/AddressBookApplication.java
@@ -1,5 +1,7 @@
 package com.apps.addressbooksystem;
 
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,10 +15,49 @@ public class AddressBookApplication {
 		System.out.println("Welcome to Address Book Program");
 
 		AddressBook addressBook = new AddressBook();
+		Scanner sc=new Scanner(System.in);
 
-		addressBook.addContact();
-		addressBook.editContact();
-		addressBook.deleteContact();
+		boolean running = true;
+
+		while (running) {
+
+		    System.out.println("1 Add Contact");
+		    System.out.println("2 Edit Contact");
+		    System.out.println("3 Delete Contact");
+		    System.out.println("4 Display Contacts");
+		    System.out.println("5 Exit");
+
+		    System.out.print("Enter your choice: ");
+		    int choice = sc.nextInt();
+		    sc.nextLine();
+
+		    switch (choice) {
+
+		        case 1:
+		            addressBook.addContact();
+		            break;
+
+		        case 2:
+		            addressBook.editContact();
+		            break;
+
+		        case 3:
+		            addressBook.deleteContact();
+		            break;
+
+		        case 4:
+		            addressBook.displayContacts();
+		            break;
+
+		        case 5:
+		            running = false;   // stop loop
+		            break;
+
+		        default:
+		            System.out.println("Invalid choice.");
+		    }
+		}
+		sc.close();
 	}
 	
 }
--- src/main/java/com/apps/addressbooksystem/service/AddressBook.java
+++ src/main/java/com/apps/addressbooksystem/service/AddressBook.java
@@ -95,6 +95,15 @@ public class AddressBook {
 
         System.out.println("Contact not found.");
     }
+    public void displayContacts() {
+        if (contactList.isEmpty()) {
+            System.out.println("No contacts available.");
+            return;
+        }
+        for (Contact contact : contactList) {
+            System.out.println(contact);
+        }
+    }
     
 
 }
