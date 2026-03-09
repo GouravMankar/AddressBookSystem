952bdd445bd3dfd53ec4f87e8aaf90382efaadf7
GouravMankar <gouravmankar@hotmail.com>
2026-03-09 23:14:48 +0530

    [Gourav Mankar] : Ability to create a Contacts in Address Book with first and last names, address, city, state, zip, phone number and email

--- src/main/java/com/apps/addressbooksystem/AddressBookApplication.java
+++ src/main/java/com/apps/addressbooksystem/AddressBookApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.apps.addressbooksystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,5 @@ public class AddressBookApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AddressBookApplication.class, args);
 	}
-		//System.out.println("")
+	
 }
